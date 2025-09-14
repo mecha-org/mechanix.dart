ac681918f8535bfa95fd4f84fce1eecb154c5edf
ojash-actonate28 <ojas.chaudhari@actonate.com>
2025-09-14 12:48:42 +0530

    feat(widget): wheel scroll widget, list with disable feature

--- flutter/widgets/lib/widgets/sectionList/section_list_items_type.dart
+++ flutter/widgets/lib/widgets/sectionList/section_list_items_type.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 
 class SectionListItems {
-  SectionListItems(
-      {required this.title,
-      this.leading,
-      this.trailing,
-      this.titleTextStyle,
-      this.onTap,
-      this.onTapUp,
-      this.onTapDown,
-      this.onDoubleTap,
-      this.defaultTrailing = true,
-      this.backgroundColor});
+  SectionListItems({
+    required this.title,
+    this.leading,
+    this.trailing,
+    this.titleTextStyle,
+    this.onTap,
+    this.onTapUp,
+    this.onTapDown,
+    this.onDoubleTap,
+    this.defaultTrailingIcon = true,
+    this.backgroundColor,
+    this.disabled = false,
+  });
 
   final Widget? leading;
   final Widget? trailing;
-  final bool defaultTrailing;
+  final bool defaultTrailingIcon;
   final String title;
   final TextStyle? titleTextStyle;
   final GestureTapCallback? onTap;
@@ -23,4 +25,5 @@ class SectionListItems {
   final GestureTapDownCallback? onTapDown;
   final GestureTapCallback? onDoubleTap;
   final Color? backgroundColor;
+  final bool disabled;
 }
