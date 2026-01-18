e25fd4f7c5927e55cf69f070e6ac7e1e3b713328
ojas-mecha <ojasc@ext.mechasystems.com>
2026-01-18 16:23:30 +0530

    fix: bottom sheet background color

--- flutter/widgets/lib/widgets/bottom_sheet_modals/mechanix_bottom_sheet_theme.dart
+++ flutter/widgets/lib/widgets/bottom_sheet_modals/mechanix_bottom_sheet_theme.dart
@@ -5,11 +5,11 @@ import 'package:flutter/material.dart';
 class MechanixBottomSheetThemeData
     extends ThemeExtension<MechanixBottomSheetThemeData> with Diagnosticable {
   const MechanixBottomSheetThemeData({
-    this.decoration,
+    this.decoration = const BoxDecoration(),
     this.padding = EdgeInsets.zero,
   });
 
-  final BoxDecoration? decoration;
+  final BoxDecoration decoration;
   final EdgeInsetsGeometry padding;
 
   @override
