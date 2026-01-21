754a809b3dca4b3cd5627007e3d55667b1605bc6
ojas-mecha <ojasc@ext.mechasystems.com>
2026-01-21 11:19:57 +0530

    fix: remove commented code

--- flutter/widgets/lib/theme/theme_data.dart
+++ flutter/widgets/lib/theme/theme_data.dart
@@ -219,20 +219,6 @@ ThemeData createDarkTheme({
     onInverseSurface: themeColors.foreground_600,
   );
 
-  // primary: themeColors.accent_300, // rgb(181,103,0)
-  // primaryContainer: themeColors.accent_200, // rgb(198, 118, 0)
-  // secondary: themeColors.background_800, // rgb(22,22,22)
-  // secondaryContainer: themeColors.background_700, // rgb(34,34,34)
-  // surfaceContainerHighest: themeColors.background_500, // rgb(58,58,58)
-  // surfaceContainerHigh: themeColors.background_600, // rgb(45,45,45)
-  // surface: themeColors.background_1000, // rgb(0,0,0)
-  // surfaceContainer: themeColors.background_400, //rgb(71,71,71)
-  // outlineVariant: themeColors.background_0, // rgb(128,128,128)
-  // onSurface: themeColors.foreground_200, // rgb(222,222,222)
-  // onSurfaceVariant: themeColors.foreground_1000, // rgb(99,99,99)
-  // onInverseSurface: themeColors.foreground_600, // rgb(158,158,158)
-  // onSecondary: themeColors.foreground_500, // rgb(174,174,174)
-  //
   return createTheme(colorScheme: colorScheme);
 }
 
