518484094a8b13a802aacc550719f5667d6a295b
ojas-mecha <ojasc@ext.mechasystems.com>
2026-09-08 11:53:58 +0530

    feat(text-field): implement text field component

--- widgets/lib/widgets.dart
+++ widgets/lib/widgets.dart
@@ -23,6 +23,8 @@ export 'src/extensions/typography_extension.dart';
 export 'src/components/button/button_style.dart';
 export 'src/components/button/button_theme.dart';
 export 'src/components/button/button.dart';
+export 'src/components/text-field/text_field_theme.dart';
+export 'src/components/text-field/text_field.dart';
 export 'src/components/checkbox/checkbox_theme.dart';
 export 'src/components/checkbox/checkbox.dart';
 export 'src/components/icon_button/icon_button.dart';
