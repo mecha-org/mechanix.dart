4308bf5082a79125338cb34d1512dea03a3fd318
yogitah-mecha <yogitah@mechasystems.com>
2026-09-15 12:04:06 +0530

    feat: app bar widget implementation

--- example/lib/layout/app_sidebar.dart
+++ example/lib/layout/app_sidebar.dart
@@ -141,6 +141,12 @@ class AppSidebar extends StatelessWidget {
                 title: 'Switch',
                 icon: Icons.toggle_on_outlined,
               ),
+              _buildNavItem(
+                context,
+                id: 'app_bar',
+                title: 'App Bar',
+                icon: Icons.title_rounded,
+              ),
             ],
           ),
         ),
