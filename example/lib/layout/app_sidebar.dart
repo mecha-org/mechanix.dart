95bb47250e9dfbad11a9f7fb1ed461620522c5bb
ojas-mecha <ojasc@ext.mechasystems.com>
2026-09-18 17:06:47 +0530

    fix: remove duplicate entry from side bar

--- example/lib/layout/app_sidebar.dart
+++ example/lib/layout/app_sidebar.dart
@@ -159,12 +159,6 @@ class AppSidebar extends StatelessWidget {
                 title: 'App Bar',
                 icon: Icons.web_asset_rounded,
               ),
-              _buildNavItem(
-                context,
-                id: 'app_bar',
-                title: 'App Bar',
-                icon: Icons.web_asset_rounded,
-              ),
             ],
           ),
         ),
